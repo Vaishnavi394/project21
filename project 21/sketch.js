e50f97234dfbe2e9227058c1c4495120579acc74
Vaishnavi394 <85866052+Vaishnavi394@users.noreply.github.com>
2021-07-02 20:04:54 +0530

    Add files via upload

---
+++ project 21/sketch.js
@@ -0,0 +1,61 @@
+var bullet,wall,thickness;
+var speed,weight;
+
+function setup() {
+  createCanvas(1600,400);
+
+  thickness=random(22,83);
+
+  bullet=createSprite(50, 200, 50, 10);
+  wall=createSprite(1200,200,thickness,100);
+
+  speed=random(223,321);
+  weight=random(30,52);
+
+  bullet.velocityx=speed;
+
+}
+
+function draw() {
+  background( "black");
+  
+  if(wall.x.bullet.x<(bullet.width+wall.width)/2){
+    bullet.velocityx=0;
+    var deformation=0.5+weight+speed/22509;
+    if(deformation>100){
+      bullet.shapeColor="white";
+    }
+    if(deformation<180 && deformation>100){
+      bullet.shapeColor="white";
+    }
+    if(deformation<100){
+      bullet.shapeColor="white";
+    }
+  }
+  if(hasCollided(bullet,wall))
+{
+bullet.velocityx=0;
+var damage=0.5=weight=speed=speed/(thickness=thickness=thickness);
+
+if(damage>10)
+{
+  wall.shapeColor="red";
+}
+if(damage<10)
+{
+  wall.shapeColor="green";
+ }
+}
+
+
+drawSprites();
+}
+
+function hasCollided(lbullet,lwall){
+  bulletRightEdge=lbullet.x+lbullet.width;
+  wallLeftEdge=lwall.x;
+  if(bulletRightEdge>wallLeftEdge){
+    return true
+  }
+  return false;
+}
